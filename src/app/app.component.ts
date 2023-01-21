b026b2a17e441f6abe012b0870a63d33beb5bcff
Caleb Hurshman <caleb@hurshman.com>
2023-01-21 17:20:34 -0500

    gradient background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'personal-site';
+
+  constructor(
+    private readonly offcanvasService: NgbOffcanvas,
+    private readonly route: ActivatedRoute
+  ) {}
+
+  onMenuButtonClick(offcanvasTemplate: TemplateRef<string>) {
+    this.offcanvasService.open(offcanvasTemplate, { position: 'end' });
+  }
 }
